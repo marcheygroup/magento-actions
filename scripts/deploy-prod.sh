bc30478c9c6d945d723f0b87cfc56b2fa83bfa74
Milind Singh <milind7@live.com>
2023-08-25 13:51:08 +0530

    fix: selector removed

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -51,15 +51,9 @@ cd /opt/config/php-deployer
 
 echo 'Deploying production ...';
 
-DEFAULT_SELECTOR=production
-if [ -n "$SELECTOR" ]
-then
-  DEFAULT_SELECTOR=$SELECTOR
-fi
-
 echo '------> Deploying bucket ...';
 # deploy bucket
-php7.4 ./vendor/bin/dep deploy-bucket $DEFAULT_SELECTOR \
+php7.4 ./vendor/bin/dep deploy-bucket production \
 -o bucket-commit=$BUCKET_COMMIT \
 -o host_bucket_path=$HOST_DEPLOY_PATH_BUCKET \
 -o deploy_path_custom=$HOST_DEPLOY_PATH \
@@ -90,7 +84,7 @@ then
 fi
 
 # deploy release
-php7.4 ./vendor/bin/dep $DEFAULT_DEPLOYER $DEFAULT_SELECTOR \
+php7.4 ./vendor/bin/dep $DEFAULT_DEPLOYER production \
 -o bucket-commit=$BUCKET_COMMIT \
 -o host_bucket_path=$HOST_DEPLOY_PATH_BUCKET \
 -o deploy_path_custom=$HOST_DEPLOY_PATH \
